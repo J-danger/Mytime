9dec1b83a0fae90149e9c3ac943d65feefa9ba8a
Stephen Howe <Stevehowe10@gmail.com>
2020-04-29 15:18:58 -0500

    first commit, boiler plate

---
+++ src/inject/inject.js
@@ -0,0 +1,11 @@
+chrome.extension.sendMessage({}, function(response) {
+	var readyStateCheckInterval = setInterval(function() {
+	if (document.readyState === "complete") {
+		clearInterval(readyStateCheckInterval);
+
+		console.log("Hello. This message was sent from scripts/inject.js");
+		alert('works')
+	
+	}
+	}, 10);
+});
